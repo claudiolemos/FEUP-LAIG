63f381dd7e582a4c144898a211fd53df5ef79b9d
Cláudio Lemos <cflemos1999@me.com>
2018-11-22 23:32:14 +0000

    Vehicle finished

--- Project2/primitives/MyCylinder2.js
+++ Project2/primitives/MyCylinder2.js
@@ -15,29 +15,34 @@ class MyCylinder2 extends CGFobject
 	};
 
 	init() {
-		var mid = (this.base+this.top)/2;
 		var w = Math.pow(2, 0.5)/2;
-
-		console.log('BASE:' + this.base + ' TOP:' + this.top + ' HEIGHT:' + this.height + ' MID:' + mid + 'w:' + w);
-
 		this.controlvertexes = [
-			[[         0, -this.base, 0, 1], [   0, -mid, this.height/2, 1], [        0, -this.top, this.height, 1]],
-			[[ this.base, -this.base, 0, w], [ mid, -mid, this.height/2, w], [ this.top, -this.top, this.height, w]],
-			[[ this.base,          0, 0, 1], [ mid,    0, this.height/2, 1], [ this.top,         0, this.height, 1]],
-			[[ this.base,  this.base, 0, w], [ mid,  mid, this.height/2, w], [ this.top,  this.top, this.height, w]],
-			[[         0,  this.base, 0, 1], [   0,  mid, this.height/2, 1], [        0,  this.top, this.height, 1]],
-			[[-this.base,  this.base, 0, w], [-mid,  mid, this.height/2, w], [-this.top,  this.top, this.height, w]],
-			[[-this.base,          0, 0, 1], [-mid,    0, this.height/2, 1], [-this.top,         0, this.height, 1]],
-			[[-this.base, -this.base, 0, w], [-mid, -mid, this.height/2, w], [-this.top, -this.top, this.height, w]],
-			[[         0, -this.base, 0, 1], [   0, -mid, this.height/2, 1], [        0, -this.top, this.height, 1]]
+			[[        0,-this.top,this.height,1],[         0,-this.base,0,1]],
+			[[-this.top,-this.top,this.height,w],[-this.base,-this.base,0,w]],
+			[[-this.top,        0,this.height,1],[-this.base,         0,0,1]]
 		];
 
-		this.surface = new CGFnurbsSurface(8, 2, this.controlvertexes);
+		this.surface = new CGFnurbsSurface(2, 1, this.controlvertexes);
 		this.nurbsObject = new CGFnurbsObject(this.scene, this.slices, this.stacks, this.surface);
 	};
 
 	display() {
 		this.nurbsObject.display();
+
+		this.scene.pushMatrix();
+			this.scene.rotate(Math.PI/2,0,0,1);
+			this.nurbsObject.display();
+		this.scene.popMatrix();
+
+		this.scene.pushMatrix();
+			this.scene.rotate(Math.PI,0,0,1);
+			this.nurbsObject.display();
+		this.scene.popMatrix();
+
+		this.scene.pushMatrix();
+			this.scene.rotate(-Math.PI/2,0,0,1);
+			this.nurbsObject.display();
+		this.scene.popMatrix();
 	};
 
 	updateTexCoords(s,t){
--- Project2/primitives/MyVehicle.js
+++ Project2/primitives/MyVehicle.js
@@ -12,28 +12,103 @@ class MyVehicle extends CGFobject
 
 	init()
 	{
-		this.frame = new MyCylinder2(this.scene, 0.5, 0.5, 6, 30, 30);
-		this.tank = new MyCylinder2(this.scene, 0.5, 0.5, 3, 30, 30);
+		var tankNosePoints = [[-0.36,0, 0.36], [-0.65,0,    0], [-0.36,0,-0.36],
+											[    0,0, 0.65], [    0,3,    0], [    0,0,-0.65],
+											[ 0.36,0, 0.36], [ 0.65,0,    0], [ 0.36,0,-0.36]];
+
+		var flap2Points = [[0,0,0], [1,0,0],
+											 [1,1.75,0], [1,1.75,0]];
+
+		var flap1Points = [[1,1.75,0], [1,0,0],
+											 [1,1.75,0], [0,0,0]];
+
+		this.frame = new MyCylinder2(this.scene, 0.3, 0.3, 6, 30, 30);
+		this.tank = new MyCylinder2(this.scene, 0.3, 0.3, 4.5, 30, 30);
+		this.adapter = new MyCylinder2(this.scene, 0.3, 0.4, 0.25, 30, 30);
+		this.top = new MyCylinder2(this.scene, 0.4, 0.4, 1, 30, 30);
+		this.tankNose = new MyPatch(this.scene,3,3,30,30,tankNosePoints);
+		this.nose = new MyCylinder2(this.scene, 0.4, 0, 0.5, 30, 30);
+		this.flap1 = new MyPatch(this.scene,2,2,30,30,flap1Points);
+		this.flap2 = new MyPatch(this.scene,2,2,30,30,flap2Points);
 	};
 
 	display() {
+
+		// frame
 		this.scene.pushMatrix();
-			// this.scene.rotate(-Math.PI/2,1,0,0);
+			this.scene.rotate(-Math.PI/2,1,0,0);
 			this.frame.display();
 		this.scene.popMatrix();
 
+		// right tank + nose
 		this.scene.pushMatrix();
-			this.scene.translate(1,0,0);
-			// this.scene.rotate(-Math.PI/2,1,0,0);
+			this.scene.translate(0.6,0,0);
+			this.scene.rotate(-Math.PI/2,1,0,0);
 			this.tank.display();
 		this.scene.popMatrix();
 
 		this.scene.pushMatrix();
-			this.scene.translate(-1,0,0);
-			// this.scene.rotate(-Math.PI/2,1,0,0);
+			this.scene.translate(-0.6,4.5,0);
+			this.scene.scale(0.6,0.6,0.6);
+			this.tankNose.display();
+		this.scene.popMatrix();
+
+		// left tank + nose
+		this.scene.pushMatrix();
+			this.scene.translate(-0.6,0,0);
+			this.scene.rotate(-Math.PI/2,1,0,0);
 			this.tank.display();
 		this.scene.popMatrix();
 
+		this.scene.pushMatrix();
+			this.scene.translate(0.6,4.5,0);
+			this.scene.scale(0.6,0.6,0.6);
+			this.tankNose.display();
+		this.scene.popMatrix();
+
+		// adapter
+		this.scene.pushMatrix();
+			this.scene.translate(0,6,0);
+			this.scene.rotate(-Math.PI/2,1,0,0);
+			this.adapter.display();
+		this.scene.popMatrix();
+
+		// top + nose
+		this.scene.pushMatrix();
+			this.scene.translate(0,6.25,0);
+			this.scene.rotate(-Math.PI/2,1,0,0);
+			this.top.display();
+		this.scene.popMatrix();
+
+		this.scene.pushMatrix();
+			this.scene.translate(0,7.25,0);
+			this.scene.rotate(-Math.PI/2,1,0,0);
+			this.nose.display();
+		this.scene.popMatrix();
+
+		// left flap
+		this.scene.pushMatrix();
+			this.scene.translate(-1.9,0.5,0);
+			this.flap1.display();
+		this.scene.popMatrix();
+
+		this.scene.pushMatrix();
+			this.scene.translate(-1.9,0.5,0);
+			this.flap2.display();
+		this.scene.popMatrix();
+
+		// right flap
+		this.scene.pushMatrix();
+			this.scene.translate(1.9,0.5,0);
+			this.scene.rotate(Math.PI,0,1,0);
+			this.flap1.display();
+		this.scene.popMatrix();
+
+		this.scene.pushMatrix();
+			this.scene.translate(1.9,0.5,0);
+			this.scene.rotate(Math.PI,0,1,0);
+			this.flap2.display();
+		this.scene.popMatrix();
 
 	};
 
