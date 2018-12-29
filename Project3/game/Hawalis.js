3b162c64091c94267e5188b13b0395c602233efc
Cláudio Lemos <cflemos1999@me.com>
2018-12-29 19:10:47 +0000

    Finished seeds animation

--- Project3/game/Hawalis.js
+++ Project3/game/Hawalis.js
@@ -8,10 +8,11 @@ class Hawalis extends CGFobject
     // this.player1Seeds;
     // this.player2Seeds;
     // this.difficulty;
-
     this.board = new Board(scene);
-    this.cells = Array(4).fill(null).map(()=>Array(7).fill([( new Seed(scene)), (new Seed(scene)) ,  (new Seed(scene)) ,  (new Seed(scene)) ,  (new Seed(scene)) ,  (new Seed(scene)) ]));
-		this.cells[0][0][4].animation = new BezierAnimation(20*1000,[[0,0,0],[0,0.2,0.25],[0,0.2,0.75],[0,0,1]])
+    this.seeds = [];
+		this.queue = [];
+		this.fillSeeds();
+		this.move(1,5);
 	};
 
   display() {
@@ -20,20 +21,81 @@ class Hawalis extends CGFobject
   };
 
   displaySeeds(){
-    for(var i = 0; i < this.cells.length; i++)
-      for(var j = 0; j < this.cells[i].length; j++)
-        for(var k = 0; k < this.cells[i][j].length; k++)
-          if(!this.cells[i][j][k].animated){
-            this.scene.pushMatrix();
-              var offset = (i == 0 || i == 1)? 0 : 1;
-							var x = (k & 1)? 0.375 : 0.625;
-							var y = 0.1 + 0.2*~~(k/4);
-							var z = (k % 4 == 0 || k % 4 == 3)? 0.375 : 0.625;
-							this.scene.translate(j+x,y,i+z+offset)
-              this.cells[i][j][k].display();
-            this.scene.popMatrix();
-          }
-  }
+		for(var i = 1; i < this.queue.length; i++)
+			this.queue[i].display();
+
+		if(this.queue.length >= 1)
+			this.animateSeed(this.queue[0]);
+
+    for(var i = 0; i < this.seeds.length; i++)
+      for(var j = 0; j < this.seeds[i].length; j++)
+        for(var k = 0; k < this.seeds[i][j].length; k++)
+						this.seeds[i][j][k].display();
+  };
+
+	animateSeed(seed){
+		if(seed.animation != null && !seed.animation.finished){
+				this.scene.pushMatrix();
+					seed.animation.update(this.scene.delta);
+					seed.animation.apply(this.scene);
+					seed.display();
+				this.scene.popMatrix();
+		}
+		else if(seed.animation != null && seed.animation.finished){
+			seed.animation = null;
+			var next = this.getFreePosition(seed.next[0],seed.next[1]);
+			seed.coord = [next[0],next[1],next[2]];
+			this.seeds[seed.next[0]][seed.next[1]].push(seed)
+			seed.next = [];
+			this.queue.shift();
+		}
+	};
+
+	move(i,j){
+		var next = [i,j]
+		while(this.seeds[i][j].length > 0){
+			var seed = this.seeds[i][j].pop();
+			seed.next = (next = this.getNext(next[0],next[1]));
+			seed.animation = this.getAnimation(seed.coord,this.getFreePosition(next[0],next[1]));
+			this.queue.push(seed);
+		}
+	}
+
+	getFreePosition(i, j){
+		var k = this.seeds[i][j].length;
+		var offset = (i == 0 || i == 1)? 0 : 1;
+		var x = (k & 1)? 0.625 : 0.375;
+		var y = 0.1 + 0.2*~~(k/4);
+		var z = (k % 4 == 0 || k % 4 == 3)? 0.375 : 0.625;
+		return vec3.fromValues(j+x,y,i+z+offset);
+	};
+
+	getAnimation(initial, final){
+		let p = [final[0]-initial[0], final[1]-initial[1], final[2]-initial[2]];
+		return (new BezierAnimation(1000,[[0,0,0],[p[0]/4,p[1]/2+1,p[2]/4],[p[0],p[1]+1,p[2]],p]));
+	};
+
+	fillSeeds(){
+    for(var i = 0; i < 4; i++){
+			this.seeds[i] = [];
+      for(var j = 0; j < 7; j++){
+				this.seeds[i][j] = [];
+				var offset = (i == 0 || i == 1)? 0 : 1;
+				this.seeds[i][j].push(new Seed(this.scene, this.getFreePosition(i,j)));
+				this.seeds[i][j].push(new Seed(this.scene, this.getFreePosition(i,j)));
+			}}
+	};
+
+	getNext(i,j){
+		if((i == 0 || i == 2) && j == 0)
+			return [i+1,j];
+		else if((i == 1 || i == 3) && j == 6)
+			return [i-1,j];
+		else if(i == 0 || i == 2)
+			return [i,j-1];
+		else if(i == 1 || i == 3)
+			return [i,j+1];
+	};
 
   updateTexCoords(s,t){
   };
--- Project3/game/Seed.js
+++ Project3/game/Seed.js
@@ -1,20 +1,18 @@
 class Seed extends CGFobject
 {
-	constructor(scene){
+	constructor(scene, coord){
 		super(scene);
 		this.seed = new MySphere(this.scene,0.1,30,30);
-		this.animated = false;
+		this.coord = coord;
+		this.next = [];
 		this.animation;
 	};
 
 	display() {
-
-		if(this.animation != null){
-			this.animation.update(this.scene.delta);
-			this.animation.apply(this.scene);
-		}
-
-    this.seed.display();
+		this.scene.pushMatrix();
+			this.scene.translate(this.coord[0],this.coord[1],this.coord[2]);
+			this.seed.display();
+		this.scene.popMatrix();
 	};
 
 	updateTexCoords(s,t){
